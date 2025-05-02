e222209afcb108edd931b7e4bbd9e56aadbe3cf0
Bruno Canto <bruno_scanto@hotmail.com>
2025-05-02 20:41:41 -0300

    criação pagina rj - Rastreadores

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+  const toggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".header-menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+
